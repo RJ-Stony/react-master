d59e7aca688f225bcb883bf213a8c7ce1c286274
노준석 <joshua052698@gmail.com>
2024-01-05 21:07:03 +0900

    Update: Animations and pseudo selectors

--- src/App.js
+++ src/App.js
@@ -1,29 +1,49 @@
-import styled from 'styled-components';
+import styled, { keyframes } from 'styled-components';
 
-const Father = styled.div`
+const Wrapper = styled.div`
   display: flex;
 `;
 
-const Btn = styled.button`
-  color: white;
-  background-color: tomato;
-  border: 0;
-  border-radius: 15px;
+const rotationAnimation = keyframes`
+  0% {
+    transform: rotate(0deg);
+    border-radius: 0px;
+  }
+  50% {
+    border-radius: 100px;
+  }
+  100% {
+    transform: rotate(360deg);
+    border-radius: 0px;
+  }
 `;
 
-const Input = styled.input.attrs({ required: true, minLength: 10 })`
-  background-color: tomato;
+const Box = styled.div`
+  height: 200px;
+  width: 200px;
+  background-color: teal;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  animation: ${rotationAnimation} 1s linear infinite;
+  span {
+    font-size: 36px;
+    &:hover {
+      font-size: 45px;
+    }
+    &:active {
+      opacity: 0;
+    }
+  }
 `;
 
 function App() {
   return (
-    <Father as="header">
-      <Btn>Login</Btn>
-      <Btn as="a" href="/">
-        Login
-      </Btn>
-      <Input></Input>
-    </Father>
+    <Wrapper>
+      <Box>
+        <span>😀</span>
+      </Box>
+    </Wrapper>
   );
 }
 
