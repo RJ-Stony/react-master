16fbc08b7fec1664af2a728ea01d028a54f53b87
노준석 <joshua052698@gmail.com>
2024-01-06 18:44:23 +0900

    Remove: lightTheme

--- src/index.js
+++ src/index.js
@@ -10,16 +10,16 @@ const darkTheme = {
   backgroundColor: '#111',
 };
 
-const lightTheme = {
-  textColor: '#111',
-  backgroundColor: 'whitesmoke',
-};
+// const lightTheme = {
+//   textColor: '#111',
+//   backgroundColor: 'whitesmoke',
+// };
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={darkTheme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
