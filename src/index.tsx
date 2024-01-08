ec5f7e7bcefc2ecb2958245b1e6a34b4e57a9fb0
노준석 <joshua052698@gmail.com>
2024-01-08 22:54:59 +0900

    Design: Front Page

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { ThemeProvider } from 'styled-components';
-import { darkTheme, lightTheme } from './theme';
+import { theme } from './theme';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(
 
 root.render(
   <React.StrictMode>
-    <ThemeProvider theme={darkTheme}>
+    <ThemeProvider theme={theme}>
       <App />
     </ThemeProvider>
   </React.StrictMode>
--- src/theme.ts
+++ src/theme.ts
@@ -1,12 +1,7 @@
 import { DefaultTheme } from 'styled-components';
 
-export const lightTheme: DefaultTheme = {
-  bgColor: 'white',
-  textColor: 'black',
-  btnColor: 'tomato',
-};
-export const darkTheme: DefaultTheme = {
-  bgColor: 'black',
-  textColor: 'white',
-  btnColor: 'teal',
+export const theme: DefaultTheme = {
+  bgColor: '#2f3640',
+  textColor: '#f5f6fa',
+  accentColor: '#fbc531',
 };
