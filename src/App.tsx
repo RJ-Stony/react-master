28380430d8b6754d1ce09d999cf445920bb00bb7
노준석 <joshua052698@gmail.com>
2024-01-08 20:20:58 +0900

    Feat: Interface

--- src/App.tsx
+++ src/App.tsx
@@ -3,8 +3,8 @@ import Circle from './Circle';
 function App() {
   return (
     <div>
-      <Circle bgColor="teal" />
-      <Circle bgColor="tomato" />
+      <Circle borderColor="black" bgColor="teal" />
+      <Circle bgColor="tomato" text="i'm here" />
     </div>
   );
 }
--- src/Circle.tsx
+++ src/Circle.tsx
@@ -1,13 +1,36 @@
 import styled from 'styled-components';
 
-const Container = styled.div``;
+interface ContainerProps {
+  bgColor: string;
+  borderColor: string;
+}
+
+const Container = styled.div<ContainerProps>`
+  width: 200px;
+  height: 200px;
+  background-color: ${(props) => props.bgColor};
+  border-radius: 100px;
+  border: 1px solid ${(props) => props.borderColor};
+`;
 
 interface CircleProps {
   bgColor: string;
+  borderColor?: string;
+  text?: string;
 }
 
-function Circle({ bgColor }: CircleProps) {
-  return <Container />;
+function Circle({ bgColor, borderColor }: CircleProps) {
+  return <Container bgColor={bgColor} borderColor={borderColor ?? bgColor} />;
 }
 
 export default Circle;
+
+interface PlayerShape {
+  name: string;
+  age: number;
+}
+
+const sayHello = (playerObj: PlayerShape) =>
+  `Hello ${playerObj.name} you are ${playerObj.age} years old.`;
+
+sayHello({ name: 'JS', age: 27 });
