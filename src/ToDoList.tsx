3b0cd677246664753a745f7cae447651e3731813
노준석 <joshua052698@gmail.com>
2024-01-19 22:08:14 +0900

    Feat: handleSubmit

--- src/ToDoList.tsx
+++ src/ToDoList.tsx
@@ -1,16 +1,28 @@
 import { useForm } from "react-hook-form";
 
 function ToDoList() {
-  const { register, watch } = useForm();
-  console.log(watch());
+  const { register, handleSubmit, formState } = useForm();
+  const onValid = (data: any) => {
+    console.log(data);
+  };
+  console.log(formState.errors);
   return (
     <div>
-      <form>
-        <input {...register("Email")} placeholder="Email" />
+      <form onSubmit={handleSubmit(onValid)}>
+        <input {...register("Email", { required: true })} placeholder="Email" />
         <input {...register("firstName")} placeholder="First Name" />
         <input {...register("lastName")} placeholder="Last Name" />
-        <input {...register("userName")} placeholder="User Name" />
-        <input {...register("password")} placeholder="Password" />
+        <input
+          {...register("userName", { required: true, minLength: 10 })}
+          placeholder="User Name"
+        />
+        <input
+          {...register("password", {
+            required: "Password is required.",
+            minLength: { value: 5, message: "Your password is too short." },
+          })}
+          placeholder="Password"
+        />
         <input {...register("confirm")} placeholder="Confirm" />
         <button>Add</button>
       </form>
