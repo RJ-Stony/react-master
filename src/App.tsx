47921d56c41c4dcbcfea6a64d3cb7ce1da057672
노준석 <joshua052698@gmail.com>
2024-02-04 22:54:37 +0900

    Updated: Variants Circle

--- src/App.tsx
+++ src/App.tsx
@@ -9,23 +9,78 @@ const Wrapper = styled.div`
   align-items: center;
 `;
 
-const Box = styled(motion.div)`
-  width: 200px;
-  height: 200px;
+const BoxesContainer = styled.div`
+  display: flex;
+  gap: 20px;
+`;
+
+const FirstBox = styled(motion.div)`
+  width: 150px;
+  height: 150px;
   background-color: white;
   border-radius: 15px;
   box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
 `;
 
-const myVars = {
+const firstVars = {
   start: { scale: 0 },
-  end: { scale: 1, rotateZ: 360, transition: { type: "spring", delay: 0.5 } },
+  end: { scale: 1, rotateZ: 180, transition: { type: "spring", delay: 0.5 } },
+};
+
+const SecondBox = styled(motion.div)`
+  width: 150px;
+  height: 150px;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  background-color: rgba(255, 255, 255, 0.2);
+  border-radius: 30px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const secondVars = {
+  start: { opacity: 0, scale: 0.5 },
+  end: {
+    opacity: 1,
+    scale: 1,
+    transition: {
+      type: "spring",
+      duration: 1,
+      bounce: 0.5,
+      delayChildren: 0.5,
+      staggerChildren: 0.25,
+    },
+  },
+};
+
+const Circle = styled(motion.div)`
+  background-color: white;
+  height: 52.5px;
+  width: 52.5px;
+  place-self: center;
+  border-radius: 30px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const circleVars = {
+  start: { opacity: 0, y: 100 },
+  end: {
+    opacity: 1,
+    y: 0,
+  },
 };
 
 function App() {
   return (
     <Wrapper>
-      <Box variants={myVars} initial="start" animate="end" />
+      <BoxesContainer>
+        <FirstBox variants={firstVars} initial="start" animate="end" />
+        <SecondBox variants={secondVars} initial="start" animate="end">
+          <Circle variants={circleVars} />
+          <Circle variants={circleVars} />
+          <Circle variants={circleVars} />
+          <Circle variants={circleVars} />
+        </SecondBox>
+      </BoxesContainer>
     </Wrapper>
   );
 }
