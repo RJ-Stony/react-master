59b2aa5c7b6e566263fe5fc78382317a6c727d61
노준석 <joshua052698@gmail.com>
2024-02-06 21:51:36 +0900

    Feat: Animate Between Components

--- src/App.tsx
+++ src/App.tsx
@@ -19,7 +19,7 @@ const Wrapper = styled(motion.div)`
 `;
 
 const BoxesContainer = styled.div`
-  display: grid;
+  display: flex;
   margin-bottom: 20px;
   grid-template-columns: repeat(4, 1fr);
   gap: 20px;
@@ -203,32 +203,74 @@ const SeventhBox = styled(motion.div)`
 `;
 
 const seventhVars = {
-  invisible: {
-    x: 500,
-    opacity: 0,
-    scale: 0,
+  entry: (back: boolean) => {
+    return {
+      x: back ? -500 : 500,
+      opacity: 0,
+      scale: 0,
+      transition: {
+        duration: 0.3,
+      },
+    };
   },
-  visible: {
+  center: {
     x: 0,
     opacity: 1,
     scale: 1,
     transition: {
-      duration: 0.5,
+      duration: 0.3,
     },
     rotateX: 360,
   },
-  exit: {
-    x: -500,
-    opacity: 0,
-    scale: 0,
-    transition: {
-      duration: 0.5,
-    },
-    rotateX: 180,
+  exit: (back: boolean) => {
+    return {
+      x: back ? 500 : -500,
+      opacity: 0,
+      scale: 0,
+      transition: {
+        duration: 0.3,
+      },
+      rotateX: 180,
+    };
   },
 };
 
+const PrevButton = styled.button`
+  width: 100px;
+  height: 100px;
+  font-size: 22px;
+  margin-left: 120px;
+  border: none;
+  border-radius: 30px;
+  color: rgb(0, 132, 90);
+  background-color: rgba(255, 255, 255, 1);
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const EighthBox = styled(motion.div)`
+  width: 100px;
+  height: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  background-color: rgba(255, 255, 255, 1);
+  border-radius: 30px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
+const CircleForEB = styled(motion.div)`
+  background-color: #99c3e9;
+  width: 35px;
+  height: 35px;
+  box-shadow: 0 2px 3px rgba(0, 0, 0, 0.1), 0 10px 20px rgba(0, 0, 0, 0.06);
+`;
+
 function App() {
+  const [showing, setShowing] = useState(false);
+  const [visible, setVisible] = useState(1);
+  const [back, setBack] = useState(false);
+  const [clicked, setClicked] = useState(false);
+
   const wrapperBoxRef = useRef<HTMLDivElement>(null);
   const x = useMotionValue(0);
   const rotateZ = useTransform(x, [-260, 0, 420], [-360, 0, 360]);
@@ -243,10 +285,16 @@ function App() {
   );
   const { scrollYProgress } = useScroll();
   const scale = useTransform(scrollYProgress, [0, 1], [0, 1.2]);
-  const [showing, setShowing] = useState(false);
   const toggleShowing = () => setShowing((prev) => !prev);
-  const [visible, setVisible] = useState(1);
-  const nextPlease = () => setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  const nextPlease = () => {
+    setBack(false);
+    setVisible((prev) => (prev === 10 ? 10 : prev + 1));
+  };
+  const prevPlease = () => {
+    setBack(true);
+    setVisible((prev) => (prev === 1 ? 1 : prev - 1));
+  };
+  const toggleClicked = () => setClicked((prev) => !prev);
   return (
     <Wrapper style={{ background: gradient }}>
       <BoxesContainer>
@@ -268,6 +316,8 @@ function App() {
             whileDrag="drag"
           ></FourthBox>
         </WrapperBoxForFB>
+      </BoxesContainer>
+      <BoxesContainer>
         <FifthBox
           style={{ x, rotateZ, scale }}
           drag="x"
@@ -315,21 +365,34 @@ function App() {
       </BoxesContainer>
       <BoxesContainer>
         <Button onClick={nextPlease}>Next Number</Button>
-        <AnimatePresence>
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) =>
-            i === visible ? (
-              <SeventhBox
-                variants={seventhVars}
-                initial="invisible"
-                animate="visible"
-                exit="exit"
-                key={i}
-              >
-                {i}
-              </SeventhBox>
-            ) : null
-          )}
+        <AnimatePresence custom={back}>
+          <SeventhBox
+            custom={back}
+            variants={seventhVars}
+            initial="entry"
+            animate="center"
+            exit="exit"
+            key={visible}
+          >
+            {visible}
+          </SeventhBox>
         </AnimatePresence>
+        <PrevButton onClick={prevPlease}>Prev Number</PrevButton>
+      </BoxesContainer>
+      <BoxesContainer onClick={toggleClicked}>
+        <EighthBox>
+          {!clicked ? (
+            <CircleForEB
+              layoutId="circle"
+              style={{ borderRadius: 50, scale: 1.5 }}
+            />
+          ) : null}
+        </EighthBox>
+        <EighthBox>
+          {clicked ? (
+            <CircleForEB layoutId="circle" style={{ borderRadius: 0 }} />
+          ) : null}
+        </EighthBox>
       </BoxesContainer>
     </Wrapper>
   );
