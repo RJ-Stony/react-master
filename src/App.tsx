80e97d74817db5fd661986a93b9dd4a774c22890
노준석 <joshua052698@gmail.com>
2024-02-07 15:44:39 +0900

    Design: Header.tsx Logo & Item's Dot

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
     <Router>
       <Header />
       <Routes>
-        <Route path="/" element={<Home />}></Route>
         <Route path="/tv" element={<Tv />}></Route>
         <Route path="/search" element={<Search />}></Route>
+        <Route path="/" element={<Home />}></Route>
       </Routes>
     </Router>
   );
